9fa5ae811bf02bd8d7099e697129ee0609b1234f
ErickEquis <ericksantillan95.01@gmail.com>
2023-05-09 14:52:41 -0600

    Se agregan comentarios en APP

--- pokemon-app/src/pages/index.tsx
+++ pokemon-app/src/pages/index.tsx
@@ -7,22 +7,30 @@ import { PokemonListResponse, SmallPokemon } from '../../interfaces';
 import { Grid } from '@nextui-org/react';
 import { PokemonCard } from '../../components/pokemon';
 
+// 'index.tsx' es la pagina 'home' de la app
+// Modularizar codigo es la mejor opcion para mantener 'index' mas legible
 
 const inter = Inter({ subsets: ['latin'] })
 
+// Props que contiene objeto 'pokemon' con interfaz 'SmallPokemon'
 interface Props {
   pokemons: SmallPokemon[];
 }
 
+// Funcion 'Home' recibe modulos para la contruccion de la pagina.
 const Home: NextPage<Props> = ({ pokemons }) => {
 
   console.log(pokemons)
-
+  // 'return' todo lo que devolvera la app
   return (
+    // 'Layout': componente (≈ modulos)   creado que recibe como parametro 'title'
+    // Los componentes se escriben como etiquetas HTML
     <Layout title='Listado pokemons'>
-
+      
+      {/* 'Grid': componente propio de React que permite estilizar*/}
       <Grid.Container gap={ 2 } justify='flex-start'>
         {
+          // 
           pokemons.map( ( pokemon ) => (
             <PokemonCard key={ pokemon.id} pokemon={ pokemon }></PokemonCard>
           ))
@@ -34,13 +42,17 @@ const Home: NextPage<Props> = ({ pokemons }) => {
   
 }
 
+// 'getStaticProps' permite pre-renderizar la API pokemon
 export const getStaticProps: GetStaticProps = async (ctx) => {
 
+  // 'data' consume API referenciando la URL y limitandola a 151 datos
   const { data } = await pokeApi.get<PokemonListResponse>('/pokemon?limit=151')
 
   console.log(data)
 
+  // 'pokemons': mapeo de API
   const pokemons: SmallPokemon[] = data.results.map( (poke, i) => ({
+    // se 'heredan' atributos de 'SmallPokemon'
     ...poke,
     id: (i + 1),
     img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ i + 1 }.svg`
