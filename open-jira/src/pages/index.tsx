351097fbeda78eb4d3db98a574074274ef878216
ErickEquis <ericksantillan95.01@gmail.com>
2023-05-15 19:41:29 -0600

    Se actualiza proyecto open-jira. Se agrega context de entradas

--- open-jira/src/pages/index.tsx
+++ open-jira/src/pages/index.tsx
@@ -1,10 +1,40 @@
-import { Typography } from "@mui/material"
+import { Card, CardContent, CardHeader, Grid } from "@mui/material"
 import { Layout } from "../../components/layouts"
 
 export default function HomePage() {
   return (
-    <Layout title="Hola Mundo!!">
-      <Typography variant="h1" color='primary'>Hola mundo!</Typography>
+    <Layout title="Home - Open-Jira">
+
+      <Grid item xs={ 12 } sm={ 4 }>
+        <Card sx={{ height: 'calc(100vh - 100px)' }}>
+          <CardHeader title= 'Pendientes' />
+          <CardContent>
+
+          </CardContent>
+
+        </Card>
+      </Grid>
+
+      <Grid item xs={ 12 } sm={ 4 }>
+        <Card sx={{ height: 'calc(100vh - 100px)' }}>
+          <CardHeader title= 'En progueso' />
+          <CardContent>
+            
+          </CardContent>
+
+        </Card>
+      </Grid>
+
+      <Grid item xs={ 12 } sm={ 4 }>
+        <Card sx={{ height: 'calc(100vh - 100px)' }}>
+          <CardHeader title= 'Completadas' />
+          <CardContent>
+            
+          </CardContent>
+
+        </Card>
+      </Grid>
+
     </Layout>
   )
 }
